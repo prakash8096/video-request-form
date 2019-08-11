185094134190dfb3a2f48fe685dcb98cbe7a00ee
prakash <saiprakadh@gmail.com>
2019-08-11 10:39:17 +0530

    added bootstrap

--- videorequestform/form/forms.py
+++ videorequestform/form/forms.py
@@ -4,11 +4,16 @@ from django import forms
 class Vform(forms.Form):
     Name=forms.CharField(max_length=20,widget=forms.TextInput(
         attrs={
-            'placeholder':'Video Title'
+            'placeholder':'Video Title',
+            'class':"form-control"
         }
     ))
     Description=forms.CharField(widget=forms.Textarea(
         attrs={
-            'placeholder':'Video Description'
+            'placeholder':'Video Description',
+            'class':"form-control",
+            'placeholder':"Enter the text",
+            'cols':"30",
+            'rows':"10" 
         }
     ))
